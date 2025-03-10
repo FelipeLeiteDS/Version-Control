2f1487d5096103accddef6ee691d06387872e1ef
Felipe Leite <felipe.nog.leite@gmail.com>
2025-03-10 15:32:49 -0700

    Update Remote-Version-Control.sh
    Adding code to the rep

--- Remote-Version-Control.sh
+++ Remote-Version-Control.sh
@@ -1,1 +1,25 @@
+#Clone and update the repository with/ new code
+cd <correct directory>
+git clone git@github.com:FelipeLeiteDS/Version-Control.git	# Cloning repository
+ls	# Checking files
+git branch
+touch Remote-Version-Control.txt	# Creating a new file
+ls	# Checking the new file
+notepad Remote-Version-Control.txt	# Update code
+git status	# Show an untracked file
+git add Remote-Version-Control.txt	# Adding file
+git status	# Show the staged file
 
+# Link existing local repository to GitHub
+# Create the Remote repository on GitHub
+cd <folder with your new repository>
+git init # Create the .git file
+git remote add origin git@github.com:FelipeLeiteDS/Version-Control.git # Connecting local rep to remote rep
+
+# Creating a fork and proposing updates on a remote repository
+# Fork (Create a clone of the rep on GitHub)
+# Chose the owner and repository name (It creates a copy of the rep on your GitHub profile)
+# Create a new feat branch
+# Deploy your feature
+# Merge with your main
+# Pull request to the main of the original rep.
